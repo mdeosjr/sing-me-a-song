f08ba8b48b7ac6f8e9fb13d05d3639b4e3353578
Moises Junior <m.junior@eq.ufrj.br>
2022-05-07 15:15:11 -0300

    cypress tests to homepage

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('resetDatabase', () => {
+	cy.request('POST', 'http://localhost:5000/reset-database');
+});
+
+Cypress.Commands.add('addRecommendation', () => {
+	const music = {
+		name: 'Falamansa - Xote dos Milagres',
+		youtubeLink: 'https://www.youtube.com/watch?v=chwyjJbcs1Y',
+	};
+
+	cy.visit('http://localhost:3000');
+
+	cy.get('input[placeholder="Name"]').type(music.name);
+	cy.get('input[placeholder="https://youtu.be/..."]').type(music.youtubeLink);
+
+	cy.intercept('POST', 'http://localhost:5000/recommendations').as(
+		'addRecommendations'
+	);
+
+	cy.get('button').click();
+
+	cy.wait('@addRecommendations');
+});
